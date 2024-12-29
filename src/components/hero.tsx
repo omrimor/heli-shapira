0f28dd6e14d9aec895a89573bf896b94e614cdf8
Omri Mor <omrimorr@gmail.com>
2024-12-29 17:44:13 +0200

    fix: hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -32,21 +32,27 @@ export function Hero({ data }: Props) {
   const { heroTitle, heroDescription, heroImage } = readFragment(HeroFragment, data);
 
   return (
-    <section id="home">
-      <div className="h-[550px] relative overflow-hidden">
-        <Image alt={heroImage?.alt ?? ''} src={heroImage?.url!} fill className="object-cover" />
-        <Container className="relative top-1/2 -translate-y-1/2">
-          <div className="max-w-md space-y-4">
-            <article className="prose max-w-none">
-              <h1 className="text-heli-primary">{heroTitle}</h1>
-              <StructuredText data={heroDescription} />
-            </article>
-            <Button size="lg" asChild>
-              <a href="#contact">בואו נדבר</a>
-            </Button>
-          </div>
-        </Container>
+    <section className="pt-24 pb-10 overflow-hidden relative">
+      <div className="absolute inset-0 hidden md:block -translate-x-1/4">
+        <Image
+          priority
+          alt={heroImage?.alt ?? ''}
+          src={heroImage?.url!}
+          fill
+          className="object-contain"
+        />
       </div>
+      <Container className="relative">
+        <div className="max-w-xl space-y-4">
+          <article className="prose max-w-none">
+            <h1 className="text-heli-primary">{heroTitle}</h1>
+            <StructuredText data={heroDescription} />
+          </article>
+          <Button size="lg" asChild>
+            <a href="#contact">בואו נדבר</a>
+          </Button>
+        </div>
+      </Container>
     </section>
   );
 }
